6e6907a2360663662f100f55c47cabbd256014dc
quqeqkaka <153831415+quqeqkaka@users.noreply.github.com>
2024-05-04 21:15:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("openModal").addEventListener("click", function(){
+    document.getElementById("modal").classList.remove('visibleBox')
+});
+document.getElementById("closeModal").addEventListener("click",function(){
+    document.getElementById("modal").classList.add("visibleBox")
+});
